8e2fefbee2bc7f8dc50a3bee463aba747c2bc7bf
altafhussain02 <137064336+altafhussain02@users.noreply.github.com>
2023-06-21 13:00:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-fcs3ie
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-fcs3ie)
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import 'zone.js/dist/zone';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+//bootstrapApplication(App);
